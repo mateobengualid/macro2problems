6248760c1db18b12075c2dff0ac6b7ba6df1c8e1
Mateo Bengualid <benguali@ar.ibm.com>
2015-10-09 18:04:51 -0300

    Done!

--- TP2_3_d.R
+++ TP2_3_d.R
@@ -1,10 +1,14 @@
 exercise3d <- read.csv("~/Projects/facultad/MACRO2/normal_0_002.csv")
 alpha <- 0.5
 g <- 0.02
+sigma <- 0.02
 beta <- 0.95
 A0 <- 10
 L <- 1.0
-Knss <- (alpha * beta / exp(g))**(1.0/(1.0-alpha))
+Knss <- A0 * L * (alpha * beta / exp(g))**(1.0/(1.0-alpha))
+
+exercise3d$white_noise_mean_0_sd_d02 <- rep.int(0.0, nrow(exercise3d))
+exercise3d$white_noise_mean_0_sd_d02[1] <- 2*sigma
 
 exercise3d$A <- rep.int(A0, nrow(exercise3d))
 exercise3d$K <- rep.int(Knss, nrow(exercise3d))
@@ -23,9 +27,10 @@ for(i in seq(nrow(exercise3d))[-101]) {
   exercise3d$k[i] <- exercise3d$K[i] / exercise3d$A[i] / L
 }
 
+exercise3d$ln_Y <- log(exercise3d$Y)
+exercise3d$ln_y <- log(exercise3d$y)
+
 # Plot everything.
-par(mfrow=c(1,1))
-plot(exercise3d$t,log(exercise3d$y),type="l",
-     main="3.d",xlab="t",ylab="y",xaxs="i",yaxs="i",lwd=2)
-lines(exercise3d$t,log(exercise3d$Y),col="red",lwd=2)
-legend(x="bottom",legend=c("ln y", "ln Y"),lwd=c(2.5,2.5),col=c("black", "red"), lty=c(1,1))
+par(mfrow=c(1,2))
+plot(exercise3d$t[1:15],exercise3d$ln_y[1:15],type="l", main="3.d.I",ylim=c(-0.80,-0.72),xlab="t",ylab="ln y",xaxs="i",yaxs="i",lwd=2)
+plot(exercise3d$t[1:15],exercise3d$ln_Y[1:15],type="l", main="3.d.II",xlab="t",ylab="ln Y",xaxs="i",yaxs="i",lwd=2)
